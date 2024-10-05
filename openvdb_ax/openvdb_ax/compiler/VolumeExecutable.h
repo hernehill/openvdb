7e58f8eb63978c4bfe3be2ae5ddee8a49c74e59a
Tim Straubinger <tstraubinger@ilm.com>
2024-10-04 18:24:29 -0700

    Convert TestVolumeExecutable.cc from cppunit to gtest; add gtest friend declarations to VolumeExecutable
    Signed-off-by: Tim Straubinger <tstraubinger@ilm.com>

--- openvdb_ax/openvdb_ax/compiler/VolumeExecutable.h
+++ openvdb_ax/openvdb_ax/compiler/VolumeExecutable.h
@@ -298,7 +298,8 @@ public:
 
 private:
     friend class Compiler;
-    friend class ::TestVolumeExecutable;
+    
+    FRIEND_TEST(TestVolumeExecutable, testConstructionDestruction);
 
     /// @brief Constructor, expected to be invoked by the compiler. Should not
     ///   be invoked directly.
